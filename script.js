55879dacf9f8dc6f00160c271c6288dd94c1c710
ImamHossain-eng <babubpx2762@gmail.com>
2022-04-26 20:20:36 +0600

    Menu link bold

---
+++ script.js
@@ -0,0 +1,12 @@
+function showService(){
+    const servicesD = document.getElementById('serviceID');
+    servicesD.style.display="block";
+    // if(servicesD.style.display="none"){
+        
+    // }else{
+    //     servicesD.style.display="none"
+    // }
+    
+
+
+}
